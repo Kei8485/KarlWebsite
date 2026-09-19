e555a422b050994e2a6249ea14305708d05e2e47
Ynand <163649128+Kei8485@users.noreply.github.com>
2026-09-19 18:02:04 +0800

    login page design done

--- website-karl-web/src/app/app-routing.module.ts
+++ website-karl-web/src/app/app-routing.module.ts
@@ -5,17 +5,17 @@ const routes: Routes = [
   // pag gagawa ng bagong page gagawing loadComponent tas gagawing Page ung dulo
  {
     path: '',
-    redirectTo: 'dev-preview',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
-  {
-    path: 'dev-preview',
-    loadComponent: () => import('./pages/dev-preview/dev-preview.page').then( m => m.DevPreviewPage)
-  },
   // {
-  //   path: 'login',
-  //   loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  //   path: 'dev-preview',
+  //   loadComponent: () => import('./pages/dev-preview/dev-preview.page').then( m => m.DevPreviewPage)
   // },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+  },
   // {
   //   path: 'subjects',
   //   loadComponent: () => import('./pages/subjects/subjects.page').then(m => m.SubjectsPage)
--- website-karl-web/src/app/pages/login/login.page.ts
+++ website-karl-web/src/app/pages/login/login.page.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular';
+// Consolidate all Ionic imports into one line from '@ionic/angular'
+import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular';
 import { AppButtonComponent } from '../../components/atoms/app-button/app-button.component';
+import { AppInputComponent } from '../../components/atoms/app-input/app-input.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, AppButtonComponent]
+  imports: [
+  CommonModule, 
+  IonContent, 
+  IonGrid, 
+  IonRow, 
+  IonCol, 
+  AppButtonComponent, 
+  AppInputComponent
+]
 })
-export class LoginPage {}
+export class LoginPage {
+  email: string = '';
+  accessCode: string = '';
+
+  onLogin() {
+    console.log('Logging in with:', this.email, this.accessCode);
+    // Next step: We'll wire this to your Django backend!
+  }
+
+  onForgot() {
+  // navigate to your reset flow
+}
+}
